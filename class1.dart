87740616768dbb0b91c21e21bc7f2a0b5cefc922
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2024-03-05 22:12:56 +0100

    news

---
+++ class1.dart
@@ -0,0 +1,32 @@
+import "dart:math";
+
+const double xOrigin = 0;
+const double yOrigin = 0;
+
+void main(List<String> args) {
+  print("ola");
+  const double n1 = 9.0;
+  const double n2 = 9.0;
+
+  var point = new Point(3.3, 4.5);
+  var point1 = new Point(1.3, 2.5);
+
+  print(point.distanceTo(point1));
+}
+
+class Point {
+  final double x, y;
+
+  Point(this.x, this.y);
+
+  Point.origin()
+      : x = xOrigin,
+        y = yOrigin;
+
+  double distanceTo(Point other) {
+    var dx = this.x - other.x;
+    var dy = this.y - other.y;
+
+    return sqrt(dx * dx + dy * dy);
+  }
+}
